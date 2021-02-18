5ead4561217dc69fd2990bc4eff672501a46f0ef
hanan1152 <77604157+hanan1152@users.noreply.github.com>
2021-02-18 20:05:13 +0300

    create a view stores

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/stores', function () {
+    $valueReceived = request("id");
+    $offers = ["lulu" => "no Promotion", "spar" => "50% off"];
+    return view("stores", ["storeId" => $valueReceived, "offers" => $offers[$valueReceived]]);
+   });
